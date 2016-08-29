1843ecc19ea5368db249fc527b80a8bba10a2fee
Maxim Kozinenko <mkozin@softserveinc.com>
2016-08-29 18:45:47 +0300

    started Varnish-ConsulKV-Traefik rework

--- kube2pyconsul.py
+++ kube2pyconsul.py
@@ -2,7 +2,7 @@
 
 Usage:
   kube2pyconsul.py [-v <loglevel>] [--verify-ssl] [--consul-agent=<consul-uri>] [--kube-master=<kubeapi-uri>]
-  [--consul-auth=<user,pass>] [--kube-auth=<user,pass>] [--consul-token=token]
+  [--consul-auth=<user,pass>] [--kube-auth=<user,pass>] [--consul-token=token] [--traefik-kv-path=path]
   kube2pyconsul.py (-h | --help)
 
 Options:
@@ -14,6 +14,7 @@ Options:
   --kube-auth=<user,pass>      Kubernetes http auth credentials [default: None]
   --verify-ssl	       	       Verify SSL or not when connecting to APIs [default: False]
   --consul-token=token         Token for ACL enabled consul
+  --traefik-kv-path=path       root key of traefik config in Consul KV
 
 """
 from docopt import docopt
@@ -42,6 +43,7 @@ log.setLevel(level)
 consul_uri = args['--consul-agent']
 consul_auth = tuple(args['--consul-auth'].split(',')) if args['--consul-auth'] != 'None' else None
 consul_token = args['--consul-token']
+traefik_path = args['--traefik-kv-path']
 
 kubeapi_uri = args['--kube-master']
 kube_auth = tuple(args['--kube-auth'].split(',')) if args['--kube-auth'] != 'None' else None
@@ -51,30 +53,6 @@ verify_ssl = args['--verify-ssl']
 log.info("Starting with: consul={0}, kubeapi={1}".format(consul_uri, kubeapi_uri))
 
 
-def getservice(event, ports):
-    if event['object']['metadata']['name'] == "kubernetes":
-        return "K8s"
-    elif 'nodePort' in event['object']['spec']['ports'][0]:
-        key, value = event['object']['spec']['selector'].items()[0]
-        r = requests.get('{base}/api/v1/pods?labelSelector={key}={value}'.format(base=kubeapi_uri,
-                                                                                 key=key, value=value),
-                         auth=kube_auth, verify=verify_ssl)
-        pod_json = json.loads(r.content)
-        return {
-                "ID": '{}-{}'.format(event['object']['metadata']['name'],
-                                     event['object']['spec']['ports'][0]['nodePort']),
-                "Name": event['object']['metadata']['name'],
-                "Address": pod_json['items'][0]['status']['hostIP'],
-                "Port": event['object']['spec']['ports'][0]['nodePort'],
-                "Check": {
-                          "TCP": "localhost:" + str(event['object']['spec']['ports'][0]['nodePort']),
-                          "Interval": "10s"
-                          }
-                }
-    else:
-        return "NEXP"
-
-
 def get_kube_hosts():
     request_url = kubeapi_uri + '/api/v1/nodes'
     while True:
@@ -82,14 +60,28 @@ def get_kube_hosts():
             ip_list = {}
             r = requests.get(request_url, verify=verify_ssl, auth=kube_auth)
             nodes = json.loads(r.content)
-            for hosts in nodes['items']:
-                ip_list['IP'] = hosts['status']['addresses'][0]['address']
+            for index, hosts in enumerate(nodes['items']):
+                ip_list[index] = hosts['status']['addresses'][0]['address']
+            return ip_list
             break
         except Exception as e:
             log.debug(traceback.format_exc())
             log.error(e)
             log.error("Error getting nodes from KubeAPI. restarting.")
             time.sleep(10)
+
+
+def getservice(event, ports):
+    if event['object']['metadata']['name'] == "kubernetes":
+        return "K8s"
+    elif 'nodePort' in event['object']['spec']['ports'][0]:
+        hosts = get_kube_hosts()
+        resp = {}
+        for index, host in enumerate(hosts):
+            resp[hosts[index]] = 'http://' + str(hosts[index]) + ':' + str(event['object']['spec']['ports'][0]['nodePort'])
+        return json.dumps(resp)
+    else:
+        return "NEXP"
 
 
 def services_monitor(queue):
@@ -143,18 +135,29 @@ def registration(queue):
                     if service != "K8s" and service != "NEXP":
                         r = ''
                         agent_base = consul_uri
+                        app = event['object']['metadata']['name']
                         while True:
                             try:
+                                service_dict = json.loads(service)
+                                print service_dict
                                 if consul_token:
-                                    r = requests.post('{base}/v1/agent/service/register?token={token}'
-                                                      .format(base=agent_base, token=consul_token),
-                                                      json=service, auth=consul_auth, verify=verify_ssl,
-                                                      allow_redirects=True)
+                                    for hosts in service_dict:
+                                        print hosts
+                                        r = requests.put('{base}/v1/kv/{traefik}/backends/{app_name}/servers/{host}/url?token={token}'
+                                                         .format(base=agent_base, token=consul_token,
+                                                                 traefik=traefik_path, app_name=app,
+                                                                 host=hosts),
+                                                         json=service_dict[hosts], auth=consul_auth, verify=verify_ssl,
+                                                         allow_redirects=True)
                                 else:
-                                    r = requests.post('{base}/v1/agent/service/register'
-                                                      .format(base=agent_base),
-                                                      json=service, auth=consul_auth, verify=verify_ssl,
-                                                      allow_redirects=True)
+                                    for hosts in service_dict:
+                                        print hosts
+                                        r = requests.put('{base}/v1/kv/{traefik}/backends/{app_name}/servers/{host}/url'
+                                                         .format(base=agent_base,
+                                                                 traefik=traefik_path, app_name=app,
+                                                                 host=hosts),
+                                                         json=service_dict[hosts], auth=consul_auth, verify=verify_ssl,
+                                                         allow_redirects=True)
                                 break
                             except Exception as e:
                                 log.debug(traceback.format_exc())
