ef680b124b09c908b40486fcbe37bf081e939f94
Maxim Kozinenko <mkozin@softserveinc.com>
2016-09-05 15:33:22 +0300

    get_node_port fix

--- kube2pyconsul.py
+++ kube2pyconsul.py
@@ -85,8 +85,8 @@ def get_service(event):
 
 
 def get_node_port(appname):
-    r = requests.get('{base}/api/v1/services?fieldSelector="metadata.name"="{value}"'.format(base=kubeapi_uri,
-                                                                                             value=appname))
+    r = requests.get('{base}/api/v1/services?fieldSelector=metadata.name={value}'.format(base=kubeapi_uri,
+                                                                                         value=appname))
     service_dict = json.loads(r.content)
     print service_dict
     try:
